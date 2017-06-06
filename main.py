419d6633cac45eeb1037aa4aa97fe6dc38ec1e19
Suvadeep Hajra <suvadeep.hajra@gmail.com>
2017-06-06 20:30:33 +0530

    Latest with error

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from DataParser import DataParser
 
 maxParagraphLength=250
 maxParagraphs=10
-labels=10
+labels=1000
 vocabularySize=150000
-model = Model2(maxParagraphLength,maxParagraphs,labels, vocabularySize)
-training = DataParser(maxParagraphLength,maxParagraphs,labels)
-training.getDataFromfile("data/vocab_3L_l10_red_train.txt")
+model = Model2(maxParagraphLength,maxParagraphs,labels,vocabularySize)
+training = DataParser(maxParagraphLength,maxParagraphs,labels,vocabularySize)
+training.getDataFromfile("data/vocab_3L_l1000_red_train.txt")
 
-testing = DataParser(maxParagraphLength,maxParagraphs,labels)
-testing.getDataFromfile("data/vocab_3L_l10_red_test.txt")
+testing = DataParser(maxParagraphLength,maxParagraphs,labels,vocabularySize)
+testing.getDataFromfile("data/vocab_3L_l1000_red_test.txt")
 
 epoch=5
 for e in range(epoch):
@@ -26,7 +26,7 @@ for e in range(epoch):
     for itr in range(training.totalPages):
         cost=model.train(training.nextBatch())
     print str(cost)
-    model.save("model2_l10_"+str(e))
+    model.save("model2_l1000_"+str(e))
 
 '''
 model.load("results/model2_9")
